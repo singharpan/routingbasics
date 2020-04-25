7b6e0b7a57366020fbf064ac13b14c974c841c5c
Bucky <singharpan09@gmail.com>
2020-04-25 20:25:10 +0530

    enclosed react elements in router

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import Navar from "./navbar";
 import Home from "./home";
 import Products from "./products";
 import Contact from "./contact";
-import { BrowserRouter } from "react-router-dom"; //to use router on browser
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
-    <div>
-      <Navar />
-      <Home />
-      <Products />
-      <Contact />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Navar />
+        <Home />
+        <Products />
+        <Contact />
+      </div>
+    </BrowserRouter>
   );
 }
-
+//Now we can use routing to these react elements
 export default App;
